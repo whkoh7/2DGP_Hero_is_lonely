01f6d2ccfdae23c64a7b6dd821b76d90aec3affe
WooHyuk Kho(Laptop) <whkoh7@naver.com>
2019-11-22 16:56:27 +0900

    a

--- Hero_is_lonely/Cards.py
+++ Hero_is_lonely/Cards.py
@@ -1,5 +1,4 @@
 from plistlib import Data
-
 from Hero import Hero_Class
 from Monster import Monster_Class
 from pico2d import *
@@ -15,18 +14,22 @@ class Card:
         self.Available = False  # 덱에 들어가있는지
         self.exposed = False  # 핸드에 있는지
         # self.H_HP, self.H_ATK, self.H_DEF, self.M_HP, self.M_ATK, self.M_DEF = 0, 0, 0, 0, 0, 0
-        
+
+    def get_bb(self):
+        return self.x - 100, self.y - 140, self.x + 100, self.y + 140
 
     def update(self):
         pass
 
     def click(self):
-        self.image = load_image('img/Card_Sample_Big.png')
-        self.clicked = True
+        if self.clicked == False:
+            self.y += 50
+            self.clicked = True
 
-    def Click_Up(self):
-        self.image = load_image('img/Card_Sample_Small.png')
-        self.clicked = False
+    def click_up(self):
+        if self.clicked:
+            self.y -= 50
+            self.clicked = False
 
     def Card_In_Hand(self):
         pass
@@ -40,3 +43,4 @@ class Card:
 
     def draw(self):
         self.image.draw(self.x, self.y)
+        draw_rectangle(*self.get_bb())
--- Hero_is_lonely/Hero.py
+++ Hero_is_lonely/Hero.py
@@ -12,6 +12,9 @@ class Hero_Class:
         self.frame = 0
         self.hp = 100
         self.attack = 50
+        self.Text = load_font('ALGER.TTF', 30)
+        self.defense = 30
+        self.tick = 0
         self.defense=30
         self.Text = load_font('ALGER.TTF', 30)
         pass
@@ -20,9 +23,10 @@ class Hero_Class:
         pass
 
     def update(self):
-        self.frame = (self.frame + 1) % 2
-        self.x += 0
-        delay(0.1)
+        self.tick+=1
+        if self.tick > 100:
+            self.frame = (self.frame + 1) % 2
+            self.tick = 0
         pass
 
     def draw(self):
