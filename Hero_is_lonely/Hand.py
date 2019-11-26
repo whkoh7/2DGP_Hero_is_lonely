86c9149b3c7b3fc99e50009e7b2ae67b862c6cea
whkoh7(desktop) <whkoh7@naer.com>
2019-11-26 22:42:06 +0900

    Update Hand.py

--- Hero_is_lonely/Hand.py
+++ Hero_is_lonely/Hand.py
@@ -2,8 +2,9 @@ from plistlib import Data
 from pico2d import *
 
 from Card_Deck import Deck
+from Cards import Card
 import main_state
 
 class Hand:
     def __init__(self):
-        self.num=5-1    #카드 총 개수 : 5개
+        self.num=5    #카드 총 개수 : 5개
